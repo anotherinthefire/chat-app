d2f578933e1499ecc3d8963d25aa335368a19ebe
anotherinthefire <rongodfreyultra@gmail.com>
2023-07-31 11:37:33 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// import firebase from 'firebase/app'
+// import 'firebase/auth'
+// import 'firebase/firestore'
+
+import Home from './pages/Home'
+import Login from './auth/Login'
+import Signup from './auth/Signup'
+
+// firebase.initializeApp({
+
+// })
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Routes>
+      {/* create condition if no user found then redirect to login */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+
+      <Route path="/" element={<Home />} />
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+const Home = () => {
+
+  return (
+    <>
+    <Link to="/login" className="flex justify-center items-center h-screen">Login</Link>
+    </>
+  )
+}
+
+export default Home
