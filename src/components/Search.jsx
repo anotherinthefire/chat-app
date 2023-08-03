08752d29c793b9402f36f93042aca95c7d8dd192
anotherinthefire <rongodfreyultra@gmail.com>
2023-08-03 21:09:58 +0800

    bugged

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react"
-import { collection, getDocs, query, where } from "firebase/firestore";
+import { collection, getDocs, getDoc, query, serverTimestamp, setDoc, updateDoc, where } from "firebase/firestore";
 import { db } from "../firebase"
+import AuthContext from "../context/AuthContext"
 
 const Search = () => {
   const [username, setUsername] = useState("")
   const [user, setUser] = useState(null)
   const [err, setErr] = useState(false)
+
+  const {currentUser} = useContext(AuthContext)
 
   const handleSearch = async () => {
     const q = query(
@@ -26,6 +29,38 @@ const Search = () => {
     e.code === "Enter" && handleSearch()
   }
 
+  const handleSelect = async () => {
+    const combinedId = 
+    currentUser.uid > user.uid 
+    ? currentUser.uid + user.uid 
+    : user.uid + currentUser.uid
+    try{
+      const res = await getDoc(doc(db, "chats", combinedId))
+
+      if(!res.exists()){
+        await setDoc(doc(db, "chats", combinedId), { messages: [] })
+
+        await updateDoc(doc(db, "userChats", currentUser.uid),{
+          [combinedId+".userInfo"]: {
+            uid:user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL
+          },
+          [combinedId+".date"]:serverTimestamp()
+        })
+
+        
+        await updateDoc(doc(db, "userChats", user.uid),{
+          [combinedId+".userInfo"]: {
+            uid:currentUser.uid,
+            displayName: currentUser.displayName,
+            photoURL: currentUser.photoURL
+          },
+          [combinedId+".date"]:serverTimestamp()
+        })
+      }
+    }catch(err){}
+  }
   return (
     <div className="text-white">
       <div className="p-2 border-b border-gray-400">
@@ -40,7 +75,7 @@ const Search = () => {
 
       {err && <span>User not found!</span>}
 
-      {user && <div className="border-b border-gray-400 p-2 flex items-center gap-2 text-white cursor-pointer hover:bg-blue-950">
+      {user && <div className="border-b border-gray-400 p-2 flex items-center gap-2 text-white cursor-pointer hover:bg-blue-950" onClick={handleSelect}>
         <img src={user.photoURL}
           className="h-12 w-12 rounded-full object-cover" />
         <div className="">
